6126d379de9431b19ec54c73a7194da88df26b96
lukaw3d <lukaw3d@gmail.com>
2019-07-05 00:30:21 +0200

    Move CSP header into a file and add test that violates CSP

--- browser-test.js
+++ browser-test.js
@@ -1,60 +1,97 @@
-const puppeteer = require('puppeteer');
+/*
+    To use:
+        npm install --no-package-lock
+        npm run test
 
+    Puppeteer api:
+        https://github.com/GoogleChrome/puppeteer/blob/v1.12.2/docs/api.md
+    Instructions to look at what is happening on page:
+        https://github.com/GoogleChrome/puppeteer/blob/v1.12.2/README.md#debugging-tips
+        const DEBUG = true;
+    Jest api:
+        https://jestjs.io/docs/en/api
+*/
+
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const DEBUG = false;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
 
-let page;
 let browser;
+let page;
+let pageErrors;
+let reemitPageConsole;
 
 beforeAll(async () => {
-    const debug = false;
     browser = await puppeteer.launch({
-        ...(!debug ? {
+        ...(!DEBUG ? {
             headless: true,
         } : {
             headless: false,
             devtools: true,
             slowMo: 1000,
         }),
+
         args: ['--no-sandbox'], // Needed on TravisCI
         defaultViewport: { width: 1280, height: 720 },
     });
+
     page = await browser.newPage();
-    page.on('console', (msg) => console.log('browser console:', msg.text()));
-    page.on('pageerror', (err) => console.log('page pageerror:', err.toString()));
-    page.on('error', (err) => console.log('page error:', err.toString()));
+
+    page.on('console', (msg) => {
+        if (msg.type() === 'error') pageErrors.push(msg.text());
+        if (reemitPageConsole) console.log('browser console:', msg.text());
+    });
+    page.on('pageerror', (err) => {
+        pageErrors.push(err.toString());
+        if (reemitPageConsole) console.log('page error:', err.toString());
+    });
+    page.on('error', (err) => {
+        pageErrors.push(err.toString());
+        if (reemitPageConsole) console.log('page error:', err.toString());
+    });
+});
+
+beforeEach(() => {
+    pageErrors = [];
+    reemitPageConsole = true;
 });
 
 
-/*
-    To use:
-    npm install --no-package-lock
-    npm run test
+describe('Content-Security-Policy', () => {
+    it('Opening local file with strict CSP should log violations', async () => {
+        const originalContent = String(await fs.promises.readFile('./a.html'));
+        expect(originalContent).toContain('<head>');
 
-    Instructions to look at what is happening on page:
-        https://github.com/GoogleChrome/puppeteer/blob/v1.12.2/README.md#debugging-tips
-    Puppeteer api:
-        https://github.com/GoogleChrome/puppeteer/blob/v1.12.2/docs/api.md
-*/
+        const content = originalContent.replace('<head>', `<head>
+            <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'report-sample';">
+        `);
 
-describe('Test in browser', () => {
-    it('Open local file', async () => {
-        await page.goto(__dirname + '/a.html');
-        await page.click('[data-target="#demo-wgs-description"]');
+        expect(pageErrors).toEqual([]);
+        reemitPageConsole = false;
+        await page.reload();
+        await page.setContent(content);
+        reemitPageConsole = true;
+        expect(pageErrors[0]).toContain('violates the following Content Security Policy');
+    });
 
-        expect(await page.$('[href="http://trace.ncbi.nlm.nih.gov/Traces/sra/?study=SRP001534"]')).toBeTruthy();
+    it('Opening local file with whitelisted CSP hashes should work', async () => {
+        const originalContent = String(await fs.promises.readFile('./a.html'));
+        const cspHeader = String(await fs.promises.readFile('./csp_header.txt'));
+        const cleanCspHeader = cspHeader.replace(/#.*/g, '').replace(/[\n\r]/g, ' ');
 
-        const b = await page.evaluate(async () => await document.body.innerHTML);
-        console.log(b);
+        const content = originalContent.replace('<head>', `<head>
+            <meta http-equiv="Content-Security-Policy" content="${cleanCspHeader}">
+        `);
 
-        const x = await page.evaluate(async () => await window.x);
-        console.log(x);
+        expect(pageErrors).toEqual([]);
+        await page.reload();
+        await page.setContent(content);
+        expect(pageErrors).toEqual([]);
+
+        const x = await page.evaluate(() => window.x);
         expect(x).toBe(3);
-
-        /*
-        And if inline CSP sha exception doesn't match the script it would print:
-            console.log browser-test.js:21
-                browser console Refused to execute inline script because it violates the following Content Security Policy directive: "script-src 'self'". Either the 'unsafe-inline' keyword, a hash ('sha256-hVqSdzYGZPpjolc+CHBmrtFq2PjX56dsP8btshGEqt8='), or a nonce ('nonce-...') is required to enable inline execution.
-        */
     });
 });
 
